03344e020ccb0fac837f9e0399967b216fb82750
Lucas Rego <lucas-rego@dti.desenv>
2019-12-18 13:55:52 -0300

    teste

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <card-conteudo-vue perfil="http://materializecss.com/images/yuna.jpg"
+  nome="Maria Silva"
+  data="13/01/18 13:30">
+
+
+  <card-detalhe-vue img="http://materializecss.com/images/sample-1.jpg"
+    titulo=""
+    txt="I am a very simple card. I am good at containing small bits of information.
+    I am convenient because I require little markup to use effectively."  />
+
+
+  </card-conteudo-vue>
+</template>
+
+<script>
+import CardConteudoVue from '@/components/social/CardConteudoVue'
+import CardDetalheVue from '@/components/social/CardDetalheVue'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  },
+  components:{
+    CardConteudoVue,
+    CardDetalheVue
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
